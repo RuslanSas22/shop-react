ee2c2eac9e7a0248db450c6eec9d799f50a7522a
“Ruslan <ruslansas22@gmail.comgit config --global user.emailgit config --global user.email ruslansas22@gmail.com>
2023-05-30 13:13:23 +0300

    create init structure of project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import MainPage from "./components/main-page/MainPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <MainPage />
+      <Footer />
     </div>
   );
 }
